6588acd00e4d3e8e091a879138919c04769cddfc
angelina-tsuboi <angelina.t1832@gmail.com>
2020-11-04 19:59:35 -0800

    started README

--- 74HC595_Shift_Register.ino
+++ 74HC595_Shift_Register.ino
@@ -1,13 +1,10 @@
-int latchPin = 5;  // Latch pin of 74HC595 is connected to Digital pin 5
-int clockPin = 6; // Clock pin of 74HC595 is connected to Digital pin 6
-int dataPin = 4;  // Data pin of 74HC595 is connected to Digital pin 4
+int latchPin = 5;  // Latch pin of 74HC595 is connected to  pin 5
+int clockPin = 6; // Clock pin of 74HC595 is connected to pin 6
+int dataPin = 4;  // Data pin of 74HC595 is connected to pin 4
 
 byte leds = 0;    // Variable to hold the pattern of which LEDs are currently turned on or off
 
-/*
- * setup() - this function runs once when you turn your Arduino on
- * We initialize the serial connection with the computer
- */
+
 void setup() 
 {
   // Set all the pins of 74HC595 as OUTPUT
@@ -16,15 +13,13 @@ void setup()
   pinMode(clockPin, OUTPUT);
 }
 
-/*
- * loop() - this function runs over and over again
- */
+
 void loop() 
 {
   leds = 0; // Initially turns all the LEDs off, by giving the variable 'leds' the value 0
   updateShiftRegister();
   delay(500);
-  for (int i = 0; i < 8; i++) // Turn all the LEDs ON one by one.
+  for (int i = 0; i < 5; i++) // Turn all the LEDs ON one by one.
   {
     bitSet(leds, i);    // Set the bit that controls that LED in the variable 'leds'
     updateShiftRegister();
---
+++ README.md
@@ -0,0 +1,25 @@
+# 74HC595 Shift Register Light Display
+
+This projects utilizes a 74HC595 shift register to make a display with LEDS!
+
+## Materials
+- 20 - 30 jumper wires
+- 74HC595 shift register
+- 5 x 220ohm resistors
+- 5 LEDs
+- Arduino Uno
+- Breadboard
+
+## Schematic
+
+![design image](https://github.com/angelina-tsuboi/Morse_Code_Translator/blob/main/images/design.png)
+
+
+## How It Works
+
+
+## Completed Project
+
+![project photo](https://github.com/angelina-tsuboi/Morse_Code_Translator/blob/main/images/final.jpg)
+
+
